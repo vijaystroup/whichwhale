c32f7613240c067afa991bbb3fd20c821b1d9e7a
Vijay Stroup <vijaystroup@gmail.com>
2020-09-12 18:46:33 -0400

    flask terms route

--- app.py
+++ app.py
@@ -10,6 +10,9 @@ def home():
     client = request.remote_addr
     return render_template('home.html', hostname=hostname, client=client)
 
+@app.route('/terms')
+def terms():
+    return render_template('terms.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
