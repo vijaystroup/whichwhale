4c5489a665e59006db56c08377ce09b0b8a6ea35
Vijay Stroup <vijaystroup@gmail.com>
2020-09-12 15:45:52 -0400

    getting hostname

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
+import socket
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
-    return 'Hello World'
+    hostname = socket.gethostname()
+    return f'Hello World: {hostname}'
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
